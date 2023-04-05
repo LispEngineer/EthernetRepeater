b2db735d4c5dccc5d419af82cae7a4202f53e5a3
Douglas P. Fields, Jr <symbolics@lisp.engineer>
2023-04-05 13:08:44 -0400

    UNTESTED. Start building bogus packet receiver.

--- rgmii_rx.md
+++ rgmii_rx.md
@@ -33,3 +33,42 @@ Misc notes:
   * FIFO length is the same as the # of buffer entries.
 * The receiver can take stuff out of the FIFO whenever
   it wants. :)
+
+
+Testing/Debugging:
+
+* There is a "bogus" function you can enable that generates
+  packets randomly, to test RAM & FIFO functionality
+  as well as top level functionality, if desired, without
+  being connected to any PHY.
+* There is an test_rgmii_rx testing module that tests the
+  top level (bogus enabled recommended). This uses another
+  module, the test_packet_receiver that reads the FIFO and
+  just makes sure that the
+  RAM is readable and has the expected "bogus" contents.
+
+# TODO
+
+* Implement the handling of RX_DV off, to determine the
+  input speed, duplex and carrier status. See:
+  * RGMII 2.0 Spec Table 4
+  * Marvell Datasheet 2.2.3.2
+  * Output the status as signals; speeds as 1-hot
+
+# Notes on RGMII 2.0 Spec
+
+* See Table 4 for when RX_DV and _ERR are 0:
+  * Link up/down
+  * Clock speed: 2.5, 25, 125
+  * Duplex
+  * --> We can use this to decide to read DDR nor not before
+    any particular packet! So, we can remove the ddr flag input?
+  * Marvell 88E1111 Datasheet (Rev. M) implies that they do the
+    optional in-band signaling in section 2.2.3.2.
+
+# Open Questions
+
+* When RX_DV (first half of RX_CTL) goes true, does that mean
+  we need to sample the data that same exact cycle, for receipt?
+  If so, that makes our state machine a little interesting, as we
+  are in one state but doing the next state's work.
--- rgmii_rx_impl.sv
+++ rgmii_rx_impl.sv
@@ -69,6 +69,22 @@ module rgmii_rx_impl #(
   // Debugging outputs
 );
 
+// Synchronizer for inputs
+localparam SYNC_LEN = 2;
+logic [(SYNC_LEN - 1):0] syncd_ddr_rx;
+logic real_ddr_rx;
+// Registered real_ddr_rx when we become activated
+logic txn_ddr;
+
+always_ff @(posedge clk_rx) begin
+  syncd_ddr_rx <= {syncd_ddr_rx[(SYNC_LEN - 2):0], ddr_rx};
+end // synchronizer
+
+
+
+`define BOGUS
+`ifdef BOGUS
+
 ///////////////////////////////////////////////////////////////////
 // Bogus RGMII Receiver to test the RAM and FIFO
 
@@ -80,10 +96,59 @@ module rgmii_rx_impl #(
 //    with the same byte as the packet number + 0x21 (ASCII 0)
 // 5. Write a real CRC 4 bytes
 // 6. Pass the packet to the FIFO
+//    a. If the FIFO is full, increment a dropped packet
+//       counter instead
+//    b. ALTERNATIVE: Wait N cycles for FIFO to drain,
+//       simulating a typical interpacket delay, before
+//       giving up and dropping the packet.
 // 7. Repeat
 
+localparam S_WAIT = 3'd0,
+           S_HEADER = 3'd1,
+           S_DATA = 3'd2,
+           S_CRC = 3'd3,
+           S_FIFO = 3'd4;
 
-// RGMII Receiver /////////////////////////////////////////////////
+logic [2:0] state;
+logic [BUFFER_NUM_ENTRY_BITS-1:0] cur_buf;
+logic [BUFFER_ENTRY_SZ-1:0] byte_pos;
+// A count used within any state for its own purposes
+logic [BUFFER_ENTRY_SZ-1:0] local_count;
+
+always_ff @(posedge clk_rx) begin
+
+  if (reset) begin
+
+  end else begin // !reset
+
+    case (state)
+    S_WAIT: begin
+    end // S_WAIT
+    S_HEADER: begin
+    end // S_HEADER
+    S_DATA: begin
+    end // S_DATA
+    S_CRC: begin
+    end // S_CRC
+    S_FIFO: begin
+    end // S_FIFO
+    default: begin
+      state <= S_WAIT;
+    end // default
+    endcase // State machine
+
+  end // reset or not
+
+end // Bogus State Machine
+
+
+`else // NOT BOGUS
+
+////////////////////////////////////////////////////////////////////////
+// RGMII Receiver
+
+
+`endif // NOT BOGUS
 
 endmodule // rgmii_rx
 
